5928196f07c8fd36a5863da7358dc07e3910c55e
xuzhy <xuzhy@acca.com.cn>
2020-11-11 17:25:54 +0800

    创建项目，建立新的公共头部组件

---
+++ src/views/Register.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <login-top :middleTop="title"></login-top>
+  </div>
+</template>
+
+<script>
+import LoginTop from '@/components/common/LoginTop.vue';
+export default {
+  data(){
+    return{
+      title:'注册Bilibili'
+    }
+  },
+  components:{
+    LoginTop
+  }
+}
+</script>
+
+<style lang="less">
+  
+</style>
